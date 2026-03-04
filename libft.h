012d8a6240e5c28f36ef428f7cd5fdc45bb109df
creespie <150524904+creespie@users.noreply.github.com>
2026-03-04 15:45:27 +0100

    subj and lsts

--- libft.h
+++ libft.h
@@ -38,23 +38,33 @@ void	*ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strjoin(char const *s1, char const *s2);
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t ft_strlen(char *str);
+size_t	ft_strlen(char *str);
 char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char *ft_strncat(char *dest, char *src, unsigned int nb);
-int ft_strncmp(char *s1, char *s2, unsigned int n);
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+int 	ft_strncmp(char *s1, char *s2, unsigned int n);
 char    *ft_strncpy(char *dest, char *src, unsigned int n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char *ft_strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_tolower(char *str);
 char	*ft_toupper(char *str);
 
-typedef struct s_list
+typedef struct		s_list
 {
 	void			*content;
 	struct s_list	*next;
-}	t_list;
+}					t_list;
+
+t_list	*ft_lstnew(void *content);
+void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
